cf9cddb732e9e9d0769551cab3a2c1a072892514
Jit <kanetkarster@gmail.com>
2015-01-06 16:38:46 -0500

    added defaults for Session

--- src/oauth/oauth1.rs
+++ src/oauth/oauth1.rs
@@ -1,23 +1,40 @@
+use std::default::Default;
 
 pub struct Session<'a> {
-    consumer_key : &'a str,
-    consumer_secret : &'a str,
-/*
-    name : String,
-    request_token_url : String,
-    access_token_url : String,
-    authorize_url : String,
-    base_url : String,
-    session_obj : String,
-    signature_obj : String,
-*/
+    realm : Option<&'a str>,
+    oauth_token : &'a str,
+    oauth_verifier : &'a str,
+    oauth_callback : &'a str,
+    oauth_callback_confirmed : Option<&'a str>,
+    authorize_url : Option<&'a str>,
+    base_url : Option<&'a str>,
+    session_obj : Option<&'a str>,
+    signature_obj : Option<&'a str>,
+}
+
+impl<'a> Default for Session<'a> {
+    fn default() -> Session<'a> {
+        Session {
+            realm: None,
+            oauth_token : "",
+            oauth_verifier : "",
+            oauth_callback : "",
+            oauth_callback_confirmed : None,
+            authorize_url : None,
+            base_url : None,
+            session_obj : None,
+            signature_obj : None,
+        }
+    }
 }
 
 impl<'a> Session<'a> {
-    pub fn new (consumer_key: &'a str, consumer_secret: &'a str) -> Session<'a> {
+    pub fn new (token: &'a str, secret: &'a str, callback: &'a str) -> Session<'a> {
         Session {
-            consumer_key: consumer_key,
-            consumer_secret: consumer_secret
+            oauth_token: token,
+            oauth_verifier: secret,
+            oauth_callback: callback,
+            ..Default::default()
         }
     }
 }
@@ -30,6 +47,6 @@ mod tests {
     // Session initialization and setup test
     #[test]
     fn hw() {
-        let s = oauth1::Session::new("Hello", "World");
+        let s = oauth1::Session::new("Token", "Secret", "Callback");
     }
 }
