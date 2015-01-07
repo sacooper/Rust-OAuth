95377d2b9614df43d38c382d372ffeab57eb6f38
Jit <kanetkarster@gmail.com>
2015-01-07 16:01:47 -0500

    changed some names

--- src/oauth/oauth1.rs
+++ src/oauth/oauth1.rs
@@ -1,52 +1,58 @@
 use std::default::Default;
 
 pub struct Session<'a> {
-    realm : Option<&'a str>,
     oauth_token : &'a str,
     oauth_verifier : &'a str,
-    oauth_callback : &'a str,
+    request_token_url : &'a str,
+    realm : Option<&'a str>,
+    oauth_consumer_key : Option<&'a str>,
+    oauth_signature : Option<&'a str>,
+    oauth_signature_method : Option<&'a str>,
     oauth_callback_confirmed : Option<&'a str>,
-    authorize_url : Option<&'a str>,
-    base_url : Option<&'a str>,
-    session_obj : Option<&'a str>,
-    signature_obj : Option<&'a str>,
+    oauth_token_secret : Option<&'a str>,
+    temporary_credentials_url : Option<&'a str>,
+    callback_token_url : Option<&'a str>,
 }
 
 impl<'a> Default for Session<'a> {
     fn default() -> Session<'a> {
         Session {
+            oauth_token: "",
+            oauth_verifier: "",
+            request_token_url: "",
             realm: None,
-            oauth_token : "",
-            oauth_verifier : "",
-            oauth_callback : "",
-            oauth_callback_confirmed : None,
-            authorize_url : None,
-            base_url : None,
-            session_obj : None,
-            signature_obj : None,
+            oauth_consumer_key: None,
+            oauth_signature: None,
+            oauth_signature_method: None,
+            oauth_callback_confirmed: None,
+            oauth_token_secret: None,
+            temporary_credentials_url: None,
+            callback_token_url: None,
+
         }
     }
 }
-
 impl<'a> Session<'a> {
-    pub fn new (token: &'a str, secret: &'a str, callback: &'a str) -> Session<'a> {
+    pub fn new (token: &'a str, secret: &'a str, request_token_url: &'a str) -> Session<'a> {
         Session {
             oauth_token: token,
             oauth_verifier: secret,
-            oauth_callback: callback,
+            request_token_url: request_token_url,
             ..Default::default()
         }
+    }
+    pub fn get_temporary_credentials(&self) {
+
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crypto::sha1::{sha1};
-    use oauth::oauth1;
+    use oauth::oauth1::Session;
 
     // Session initialization and setup test
     #[test]
     fn hw() {
-        let s = oauth1::Session::new("Token", "Secret", "Callback");
+        let s = Session::new("Token", "Secret", "https://api.twitter.com/oauth/request_token");
     }
 }
