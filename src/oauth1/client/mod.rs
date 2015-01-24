bcb2f7b75e99b2d340cf52180d1aa4d437060ad7
Scott Cooper <scott.cooper2@mail.mcgill.ca>
2015-01-23 23:23:21 -0500

    Change thread_rng to OsRng (cryptographically secure

--- src/oauth1/client/mod.rs
+++ src/oauth1/client/mod.rs
@@ -1,11 +1,11 @@
 extern crate time;
 
 use self::time::now_utc;
-use std::rand::{thread_rng, Rng};
+use std::rand::{OsRng, Rng};
+use std::iter::Iterator;
 
 pub mod session;
 pub mod builder;
-
 
 #[derive(Copy, Show, PartialEq, Eq, Clone)]
 #[unstable]
@@ -27,7 +27,9 @@ pub trait AuthorizationHeader {
 // TODO: add to crypto library?
 // TODO: Should we have a longer nonce than 10?
 fn generate_nonce() -> String {
-    thread_rng().gen_ascii_chars()
+
+    OsRng::new().unwrap()
+                .gen_ascii_chars()
                 .take(10)
                 .collect()
 }
